ec87d40ef83234cb0ed17e0ce76409118c48fd80
Janpaa <jambex95@gmail.com>
2019-03-25 23:48:32 +0200

    Added Intro, Number wizard and text101

---
+++ Number Wizard/NumberWizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int min;
+    int max;
+    int guess;
+    int steps;
+    // Start is called before the first frame update
+    void Start(){
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        steps = 0;
+        min = 1;
+        max = 1000;
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Pick a number between " + min + " and " + max);
+        max = max + 1;
+        Debug.Log("Push Up = Higher, Push Down = Lower, Enter = Correct");
+        nextGuess();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            nextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            nextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I found your number "+guess+" in: " + steps+" steps!");
+            StartGame();
+        }
+    }
+    void nextGuess()
+    {
+        guess = Mathf.RoundToInt((min + max) / 2);
+        Debug.Log("Is it higher or lower than: " + guess);
+        steps++;
+    }
+}
---
+++ Number Wizard/NumberWizard_UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        nextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess+1;
+        nextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess-1;
+        nextGuess();
+    }
+
+    // Update is called once per frame
+    void nextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
